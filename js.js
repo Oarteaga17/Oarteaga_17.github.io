6ce3959f5fe83d1c1cf9708191809cecabd41040
Oarteaga17 <126583009+Oarteaga17@users.noreply.github.com>
2023-11-06 10:15:18 -0400

    initial commit

---
+++ js.js
@@ -0,0 +1,6 @@
+const menu = document.querySelector('.menu');
+
+menu.addEventListener('click', function () {
+    const subMenu = this.querySelector('.sub-menu');
+    subMenu.style.display = subMenu.style.display === 'none' ? 'block' : 'none';
+});
